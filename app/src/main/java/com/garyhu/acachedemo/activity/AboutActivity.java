815b652b7f563eaae9c648b92bfe652780d405fe
garyhu <hww9081020@163.com>
2017-03-10 14:16:26 +0800

    first commit

---
+++ app/src/main/java/com/garyhu/acachedemo/activity/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.garyhu.acachedemo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.garyhu.acachedemo.R;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+
+	}
+
+}
---
+++ app/src/main/java/com/garyhu/acachedemo/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package com.garyhu.acachedemo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.garyhu.acachedemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void string(View v) {
+        startActivity(new Intent().setClass(this, SaveStringActivity.class));
+    }
+
+    public void jsonobject(View v) {
+        startActivity(new Intent().setClass(this, SaveJsonObjectActivity.class));
+    }
+
+    public void jsonarray(View v) {
+        startActivity(new Intent().setClass(this, SaveJsonArrayActivity.class));
+    }
+
+    public void bitmap(View v) {
+        startActivity(new Intent().setClass(this, SaveBitmapActivity.class));
+    }
+
+    public void media(View v) {
+        startActivity(new Intent().setClass(this, SaveMediaActivity.class));
+    }
+
+    public void drawable(View v) {
+        startActivity(new Intent().setClass(this, SaveDrawableActivity.class));
+    }
+
+    public void object(View v) {
+        startActivity(new Intent().setClass(this, SaveObjectActivity.class));
+    }
+
+    public void about(View v) {
+        startActivity(new Intent().setClass(this, AboutActivity.class));
+    }
+}
